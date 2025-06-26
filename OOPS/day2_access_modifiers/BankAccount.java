be60086d2f0b107c569a9411e05699dc2d9c40ff
deepu kumar <deepu.kumar_cs23@gla.ac.in>
2025-06-26 09:24:42 +0530

    [deepu kumar] add.BankAccount

---
+++ OOPS/day2_access_modifiers/BankAccount.java
@@ -0,0 +1,30 @@
+package OOPS.day2_access_modifiers;
+class BankAccount {
+    public String accountNumber;
+    protected String accountHolder;
+    private double balance;
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+class SavingsAccount extends BankAccount {
+    public void displayAccountInfo() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Balance (via getter): " + getBalance());
+    }
+}
+
