8029e6521bacff74f3108619381984e76bce27d8
deepu kumar <deepu.kumar_cs23@gla.ac.in>
2025-06-26 09:27:12 +0530

    [deepu kumar] add.Employee

---
+++ OOPS/day2_access_modifiers/Employee.java
@@ -0,0 +1,23 @@
+package OOPS.day2_access_modifiers;
+class Employee {
+    public int employeeID;
+    protected String department;
+    private double salary;
+
+    public void updateSalary(double newSalary) {
+        if (newSalary > 0) {
+            salary = newSalary;
+        }
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
+class Manager extends Employee {
+    public void displayManagerInfo() {
+        System.out.println("Employee ID: " + employeeID);
+        System.out.println("Department: " + department);
+        System.out.println("Salary (via getter): " + getSalary());
+    }
+}
