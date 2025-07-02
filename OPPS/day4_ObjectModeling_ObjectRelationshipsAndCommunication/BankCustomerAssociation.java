3adf996f3f17d3d0fb44febc70717ac9feb12225
deepu kumar <deepu.kumar_cs23@gla.ac.in>
2025-07-02 12:18:08 +0530

    [deepu kumar] add.BankCustomerAssocition

--- OPPS/day4_ObjectModeling_ObjectRelationshipsAndCommunication/BankCustomerAssociation.java
+++ OPPS/day4_ObjectModeling_ObjectRelationshipsAndCommunication/BankCustomerAssociation.java
@@ -23,7 +23,7 @@ class Bank {
 class Customer {
     String name;
     double balance;
-    Customer(String name, double balance) {
+    Customer(String name, double balance){
         this.name = name;
         this.balance = balance;
     }
