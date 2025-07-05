7b4a196b17bcda90cfaf05fec3cb4d14db838f19
deepu kumar <deepu.kumar_cs23@gla.ac.in>
2025-07-05 06:13:20 +0530

    [deepu kumar] add.Product

---
+++ OPPS/day6_Encapsulation_Polymorphism_InterfaceAndAbstractClass/ECommercePlatform.java
@@ -0,0 +1,73 @@
+package OPPS.day6_Encapsulation_Polymorphism_InterfaceAndAbstractClass;
+abstract class Product {
+    private int productId;
+    private String name;
+    private double price;
+
+    public Product(int productId, String name, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+    }
+
+    public abstract double calculateDiscount();
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getProductInfo() {
+        return productId + " - " + name + ": ₹" + price;
+    }
+}
+
+interface Taxable {
+    double calculateTax();
+    String getTaxDetails();
+}
+
+class Electronics extends Product implements Taxable {
+    public Electronics(int id, String name, double price) {
+        super(id, name, price);
+    }
+
+    public double calculateDiscount() {
+        return getPrice() * 0.1;
+    }
+
+    public double calculateTax() {
+        return getPrice() * 0.18;
+    }
+
+    public String getTaxDetails() {
+        return "GST 18%";
+    }
+}
+
+class Clothing extends Product implements Taxable {
+    public Clothing(int id, String name, double price) {
+        super(id, name, price);
+    }
+
+    public double calculateDiscount() {
+        return getPrice() * 0.2;
+    }
+
+    public double calculateTax() {
+        return getPrice() * 0.05;
+    }
+
+    public String getTaxDetails() {
+        return "GST 5%";
+    }
+}
+
+class Groceries extends Product {
+    public Groceries(int id, String name, double price) {
+        super(id, name, price);
+    }
+
+    public double calculateDiscount() {
+        return getPrice() * 0.05;
+    }
+}
