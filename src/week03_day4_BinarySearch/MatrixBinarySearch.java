ce9836fdd981da2ee3091dcc4f7efc01a2f7f9d3
deepu kumar <deepu.kumar_cs23@gla.ac.in>
2025-07-11 19:29:23 +0530

    [deepu kumar] add.MatrixBinarySearch

---
+++ src/week03_day4_BinarySearch/MatrixBinarySearch.java
@@ -0,0 +1,29 @@
+package week03_day4_BinarySearch;
+
+public class MatrixBinarySearch {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int left = 0, right = rows * cols - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int midElement = matrix[mid / cols][mid % cols];
+
+            if (midElement == target) return true;
+            else if (midElement < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 3, 5},
+            {7, 9, 11},
+            {13, 15, 17}
+        };
+        int target = 9;
+        System.out.println("Target found: " + searchMatrix(matrix, target));
+    }
+}
