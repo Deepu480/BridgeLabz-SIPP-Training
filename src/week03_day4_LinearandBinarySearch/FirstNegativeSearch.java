0270396601f8082a131bc02178b7085a7b2b3ee4
deepu kumar <deepu.kumar_cs23@gla.ac.in>
2025-07-11 19:18:34 +0530

    [deepu kumar] add.FirstNegativeSerch

---
+++ src/week03_day4_LinearandBinarySearch/FirstNegativeSearch.java
@@ -0,0 +1,17 @@
+package week03_day4_LinearandBinarySearch;
+
+public class FirstNegativeSearch {
+    public static int findFirstNegative(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, -2, 7};
+        System.out.println("Index of first negative: " + findFirstNegative(arr));
+    }
+}
