cb8a29a87836fc079359d3494c4930db72abb420
deepu kumar <deepu.kumar_cs23@gla.ac.in>
2025-07-11 19:20:16 +0530

    [deepu kumar] add.WordSearchInsentence

---
+++ src/week03_day4_LinearandBinarySearch/WordSearchInSentences.java
@@ -0,0 +1,18 @@
+package week03_day4_LinearandBinarySearch;
+
+public class WordSearchInSentences {
+    public static String searchWord(String[] sentences, String word) {
+        for (String sentence : sentences) {
+            if (sentence.contains(word)) {
+                return sentence;
+            }
+        }
+        return "Not Found";
+    }
+
+    public static void main(String[] args) {
+        String[] sentences = {"Java is great", "I love coding", "This is linear search"};
+        String word = "love";
+        System.out.println("Sentence: " + searchWord(sentences, word));
+    }
+}
