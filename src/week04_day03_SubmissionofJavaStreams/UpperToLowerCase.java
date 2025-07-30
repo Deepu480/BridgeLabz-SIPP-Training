a146620b0b8232ad6e845ae483c2a832ea15269c
deepu kumar <deepu.kumar_cs23@gla.ac.in>
2025-07-30 18:05:53 +0530

    [deepu kumar] add.UpperToLowerCase

---
+++ src/week04_day03_SubmissionofJavaStreams/UpperToLowerCase.java
@@ -0,0 +1,20 @@
+package week04_day03_SubmissionofJavaStreams;
+
+import java.io.*;
+
+public class UpperToLowerCase {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line.toLowerCase());
+                writer.newLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
