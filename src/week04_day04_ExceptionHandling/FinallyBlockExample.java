02f6c107adbd3b150a15e4f1438981d91b255a75
deepu kumar <deepu.kumar_cs23@gla.ac.in>
2025-08-01 14:24:18 +0530

    [deepu kumar] add.FinllayBlockExample

---
+++ src/week04_day04_ExceptionHandling/FinallyBlockExample.java
@@ -0,0 +1,27 @@
+
+package week04_day04_ExceptionHandling;
+
+import java.util.Scanner;
+
+public class FinallyBlockExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter numerator: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter denominator: ");
+        int b = sc.nextInt();
+
+        try {
+            int result = a / b;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero!");
+        } finally {
+            System.out.println("Operation completed");
+            sc.close();
+        }
+    }
+}
+
