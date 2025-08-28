6b22e583dbe3c4db493f0882076af4699b242df0
deepu kumar <deepu.kumar_cs23@gla.ac.in>
2025-08-28 17:36:16 +0530

    [deepu kumar] add.ReplaceSpaces

---
+++ src/week06_day06_PracticesRegex_and_JUnit/ReplaceSpaces.java
@@ -0,0 +1,11 @@
+package week06_day06_PracticesRegex_and_JUnit;
+
+public class ReplaceSpaces {
+    public static void main(String[] args) {
+        String input = "This   is  an    example   with   multiple   spaces.";
+        String output = input.replaceAll("\\s+", " ");
+              
+        System.out.println("Original: " + input);
+        System.out.println("Processed: " + output);
+    }
+}
