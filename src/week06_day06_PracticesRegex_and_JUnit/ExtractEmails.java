b98bc781480bb16fbb437fb904d5b39920eaa912
deepu kumar <deepu.kumar_cs23@gla.ac.in>
2025-08-28 11:22:47 +0530

    [deepu kumar] add.ExtractEmails

---
+++ src/week06_day06_PracticesRegex_and_JUnit/ExtractEmails.java
@@ -0,0 +1,19 @@
+package week06_day06_PracticesRegex_and_JUnit;
+
+import java.util.regex.*; 
+
+public class ExtractEmails {
+    public static void main(String[] args) {
+        String text = "Contact us at support@example.com and info@company.org";
+        String emailRegex = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(text);
+
+        System.out.println("Extracted Email Addresses:");
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
+
