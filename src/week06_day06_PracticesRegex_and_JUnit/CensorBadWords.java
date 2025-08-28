3220b040a91daab272de79f7b1a45cd53428facc
deepu kumar <deepu.kumar_cs23@gla.ac.in>
2025-08-28 17:37:59 +0530

    [deepu kumar] add.CensorBodWords

---
+++ src/week06_day06_PracticesRegex_and_JUnit/CensorBadWords.java
@@ -0,0 +1,19 @@
+package week06_day06_PracticesRegex_and_JUnit;
+
+import java.util.*;
+
+public class CensorBadWords {
+    public static void main(String[] args) {
+        String input = "This is a damn bad example with some stupid words.";
+        List<String> badWords = Arrays.asList("damn", "stupid");
+
+        String censored = input;
+
+        for (String badWord : badWords) {
+            censored = censored.replaceAll("(?i)\\b" + badWord + "\\b", "****");
+        }
+
+        System.out.println("Original: " + input);
+        System.out.println("Censored: " + censored);
+    }
+}
